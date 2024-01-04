83200c9d1a9b0fa6ec16239540f4d07228c4d644
pnasiba <pnasiba23.06@gmail.com>
2024-01-05 01:44:19 +0500

    info about js

---
+++ lesson-1/script.js
@@ -0,0 +1,57 @@
+// 1
+// var m = prompt("Metr kiriting: ")
+// var km = m / 1000;
+// alert(km)
+
+// console.log(km)
+
+
+// 2
+// var age = prompt("Tug'ilgan yilingizni kiriting: ")
+// var year = 2024 - age;
+// alert(year)
+
+// console.log(year)
+
+
+// 3
+// let age = prompt("Enter your age: ")
+
+// if(age > 50){
+//     console.log("Siz kattasiz")
+// } else{
+//     console.log("Siz yoshsiz")
+// }
+
+
+// 4
+// let price = prompt("Enter your somsa's price: ")
+
+// if (price > 3000){
+//     console.log("Expensive")
+// } else{
+//     console.log("Cheap");
+// }
+
+// 5
+// let password = prompt("Enter your password: ")
+
+// if (password = sarimsoqpiyoz){
+//     console.log("Assalomu aleykum");
+// } else{
+//     console.log("Error");
+// }
+
+
+// 6
+// let age = prompt("Enter your age: ");
+
+// if (age >= 1 && age <= 30) {
+//   console.log("Siz yoshsiz");
+// } else if (age >= 31 && age <= 60) {
+//   console.log("Siz kattasiz");
+// } else if (age >= 61) {
+//   console.log("Siz qarisiz");
+// } else {
+//   console.log("Error");
+// }
